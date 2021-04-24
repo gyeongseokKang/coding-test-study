a3ebb7950594310887de3e2308b0a15f5f2d8f94
JangHwanBae132 <76988903+JangHwanBae132@users.noreply.github.com>
2021-04-24 15:42:18 +0900

    Update 배_튜플.java
    코드 간소화

--- solution/210421/배_튜플.java
+++ solution/210421/배_튜플.java
@@ -1,27 +1,24 @@
 import java.util.*;
 
 class Solution {
-	
-	
+    
     public static int[] solution(String s) {
         Set<Integer> set = new LinkedHashSet<Integer>();
         
-        String str = s.substring(1, s.length()-2);
-        
-        String[] strArr = str.split("},");
+        String[] strArr  = s.substring(1, s.length()-2).split("},");
         
         Arrays.sort(strArr, Comparator.comparing(String::length));
         
         for(String ss: strArr) {
-        	String sss = ss.substring(1);
         	
-        	String[] strArr2 = sss.split(",");
+        	String[] strArr2 = ss.substring(1).split(",");
+        	
         	for(int i=0; i<strArr2.length; i++) {
         		set.add(Integer.parseInt(strArr2[i]));
         	}
         }
         
-      int[] answer = new int [set.size()];
+        int[] answer = new int [set.size()];
     	Iterator it=set.iterator();
     	int cnt = 0;
     	while(it.hasNext()){
