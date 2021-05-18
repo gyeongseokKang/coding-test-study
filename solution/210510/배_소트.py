f32c53901e704edd3b1a5479ebd0b6b0004e4dce
JangHwanBae132 <76988903+JangHwanBae132@users.noreply.github.com>
2021-05-18 14:08:53 +0900

    Update and rename 배_퀵소트.py to 배_소트.py

---
+++ solution/210510/배_소트.py
@@ -0,0 +1,43 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    lesser_arr, equal_arr, greater_arr = [], [], []
+    for num in arr:
+        if num < pivot:
+            lesser_arr.append(num)
+        elif num > pivot:
+            greater_arr.append(num)
+        else:
+            equal_arr.append(num)
+    return quick_sort(lesser_arr) + equal_arr + quick_sort(greater_arr)
+
+def merge_sort(arr):
+    if len(arr) < 2:
+        return arr
+
+    mid = len(arr) // 2
+    low_arr = merge_sort(arr[:mid])
+    high_arr = merge_sort(arr[mid:])
+
+    merged_arr = []
+    l = h = 0
+    while l < len(low_arr) and h < len(high_arr):
+        if low_arr[l] < high_arr[h]:
+            merged_arr.append(low_arr[l])
+            l += 1
+        else:
+            merged_arr.append(high_arr[h])
+            h += 1
+    merged_arr += low_arr[l:]
+    merged_arr += high_arr[h:]
+    return merged_arr
+
+arr= [69, 10, 30, 2,2,31, 16, 8, 31, 22 ]
+
+
+print("quick:  ")
+print(quick_sort(arr))
+
+print("merge:  ")
+print(merge_sort(arr))
